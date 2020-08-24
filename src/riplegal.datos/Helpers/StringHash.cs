9d662547a2a90581f70f1aa6fb10abed63455ee2
gubiarpa <billy.arredondo@pucp.pe>
2020-08-23 22:14:30 -0500

    Updating Packages + Hash
    Se actualizó los paquetes del proyecto y se agregó una clase para gestionar la encriptación y validación de las contraseñas.

---
+++ src/riplegal.datos/Helpers/StringHash.cs
@@ -0,0 +1,20 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace riplegal.datos.Helpers
+{
+    public class StringHash
+    {
+        public static string HasPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
